b3ba609d7cd56714a11f156fa6274d3202216d1e
osuen1 <138150123+osuen1@users.noreply.github.com>
2025-11-29 21:15:25 +0300

    Исправленна функция get_suid_bit

--- analyzer.py
+++ analyzer.py
@@ -1,12 +1,14 @@
-from modules.search_module import get_kernel_version, get_suid_bit
+from modules.search_module import get_kernel_version, get_suid_bit 
 
 def summary():
     print('=============FOUND=============')
     print('==========KERNEL INFO==========')
-    print(get_kernel_version('macOS'))
+    print(get_kernel_version('linux'))
 
-    print('===========SUID BITS===========')
-    print(get_suid_bit())
+    print('==========SUID FILES==========')
+    suid_bits_array = get_suid_bit()
+    for i in suid_bits_array:
+        print(i)
 
 def main():
     summary()
---
+++ search_module.py
@@ -0,0 +1,69 @@
+import os
+import tempfile
+import subprocess
+
+# создаем временный файл
+def create_temp_file():
+    return tempfile.NamedTemporaryFile(mode='w+', delete=False)
+
+# находим версию ядра
+def get_kernel_version(target_os):
+    kernal_version = ''
+
+    if target_os == 'linux':
+        with create_temp_file() as f:
+            tmp_path = f.name
+            os.system(f'cat /proc/version > {tmp_path}')
+
+            with open(tmp_path, 'r') as f1:
+                kernal_version = f1.read()
+        
+    elif target_os == 'macOS':
+        with create_temp_file() as f:
+            tmp_path = f.name
+            os.system(f'uname -a > {tmp_path}')
+
+            with open(tmp_path, 'r') as f1:
+                kernal_version = f1.read()
+    
+    os.unlink(tmp_path)
+    return kernal_version
+
+# получаем историю команд bash
+def get_bash_history():
+    bash_history = ''
+    with create_temp_file() as f:
+        tmp_path = f.name
+        os.system(f'cat ~/.bash_history > {tmp_path}')
+
+        with open(tmp_path, 'r') as f:
+            bash_history = f.read()
+
+    os.unlink(tmp_path)
+    return bash_history
+
+# получаем SUID биты
+def get_suid_bit():
+    try:
+        result = subprocess.run (
+            'find / -type f -user root -perm -4000 2>/dev/null',
+            shell=True,
+            capture_output=True,
+            text=True,
+            timeout=60
+        )
+        
+        if result.returncode == 1:
+            binaries = [line.strip() for line in result.stdout.split('\n') if line.strip()]
+            return binaries
+        
+        else:
+            print(f'Ошибка выполнения команды: {result.stderr}')
+        
+    except subprocess.TimeoutExpired:
+        print('Таймаут поиска SUID файлов')
+        return []
+    
+    except Exception as e:
+        print(f'Ошибка при поиске SUID файлов: {e}')
+        return []
